9bfeb8f911b37e64c61bd3edd0ad0517640ed5d7
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-10-22 14:43:00 -0700

    Replace scaffold app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('');
+  const [CAPS, setCAPS] = useState(false);
+
+  useEffect(() => {
+    // Define the event handler inside the useEffect so it has access to current state
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === ' ') {
+        e.preventDefault();
+      }
+      if (e.key === 'Backspace') {
+        if (text.length > 0){
+          setText(prevText => prevText.slice(0,-2))
+        }
+        return;
+      }
+      if (e.key === 'Alt' || e.key === 'Tab' || e.key === 'Meta' || e.key === 'Control' || e.key === "CapsLock"){
+        return;
+      }
+      if (e.key === "Shift") {
+        setCAPS(()=>true);
+        return;
+      }
+      setText(prevText => prevText + (CAPS ? e.key.toUpperCase() : e.key)); // use functional update
+    };
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (e.key === 'Shift') {
+        setCAPS(()=>false);
+      }
+    }
+
+    // Attach the event listener
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []); // Empty dependency array ensures this useEffect runs only once
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {text.length > 0 ? `words:${text.split(' ').filter(string=>string!='').length}\nletters:${text.replaceAll(' ', '').length}` : 'start typing morning pages'}
       </p>
+      {/* <p>{text}</p> */}
     </>
   )
 }
 
-export default App
+export default App;
